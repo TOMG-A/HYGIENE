114433db82f56d373f7906fbaa4a28fdf49b617e
Tom <im.tomtomon@gmail.com>
2023-03-17 20:57:06 +0000

    modify

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+const path = require('path');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname, "../views/index.html"))
+});
+
+router.get('/patel', function(req, res, next) {
+  res.send('bindi');
 });
 
 module.exports = router;
